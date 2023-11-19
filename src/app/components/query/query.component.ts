857ec96ed5f0933474d065a9a77c4e314d58b371
Sergio Rodriguez Vasquez <sarv9208@hotmail.com>
2023-11-19 11:45:27 -0500

    integración de paginación en ejecución de consulta BigQuery

--- src/app/components/query/query.component.ts
+++ src/app/components/query/query.component.ts
@@ -20,6 +20,11 @@ export class QueryComponent implements OnInit {
   // Valores predeterminados para pag y sizePag
   pag = 0;
   sizePag = 10;
+  // variables para el maneja de paginación
+  esPrimero = true;
+  esUltimo = true;
+  totalPaginas: number[] = [];
+  totalElements: number = 0;
 
   // Lista de los datos retornados por la api BigQuery de Google
   schedulesList: SchedulesDTO[];
@@ -36,21 +41,95 @@ export class QueryComponent implements OnInit {
 
   constructor(
     private bigQueryService: BigQueryService
-  ) { 
+  ) {
     this.schedulesList = [];
   }
 
   ngOnInit(): void {
   }
 
-  // Método que ejecuta la consulta y solicita a la pai Bigquery
+  // Método que ejecuta la consulta y solicita a la api Bigquery
   ejecutarConsultar() {
 
-    this.bigQueryService.runQuery(null, null, null, null, null, null, this.pag, this.sizePag).subscribe(resp => {
+    const gameNumber = this.filtrarForm.controls['gameNumber'].value;
+    const dayNight = this.filtrarForm.controls['dayNight'].value;
+    const duration = this.filtrarForm.controls['duration'].value;
+    const status = this.filtrarForm.controls['status'].value;
+    const year = this.filtrarForm.controls['year'].value;
+    const conditiaonal = this.filtrarForm.controls['conditiaonal'].value;
+
+    this.bigQueryService.runQuery(gameNumber, dayNight, duration, status, year, conditiaonal, this.pag, this.sizePag).subscribe(resp => {
       this.schedulesList = resp.data.content;
-      
+      if (resp.success) {
+        this.esPrimero = resp.data.first;
+        this.esUltimo = resp.data.last;
+        this.totalPaginas = new Array(resp.data['totalPages']);
+        this.totalElements = resp.data.totalElements;
+      }
     });
 
+  }
+
+  // Método encargado de limpiar el formulario de filtro
+  // quitando los filtros buscados anteriormente
+  limpiarFormFilter() {
+    this.filtrarForm.reset();
+    this.filtrarForm.get('dayNight')?.setValue('');
+    this.filtrarForm.get('status')?.setValue('');
+    this.filtrarForm.get('conditiaonal')?.setValue('');
+  }
+
+  // Método encargado de cargar una página anterior y verifica si es la primer página
+  rebobinar(primero?: any) {
+    if (primero) {
+      this.pag = 0;
+    } else {
+      if (!this.esPrimero) {
+        this.pag--;
+      }
+    }
+    this.ejecutarConsultar();
+  }
+
+  // Método encargado de cargar una página seguiente y verifica si es la última página
+  avanzar(ultimo?: any) {
+    if (ultimo) {
+      this.pag = this.totalPaginas.length - 1;
+    } else {
+      if (!this.esUltimo) {
+        this.pag++;
+
+      }
+    }
+    this.ejecutarConsultar();
+  }
+
+  // Método encargado de cambiar el valor de la página actual
+  setearPag(pag: number): void {
+    this.pag = pag;
+    this.ejecutarConsultar();
+  }
+
+  // Método encargado de cambiar la cantidad de elementos por página
+  setearCant(cant: any): void {
+    if (this.totalElements > 0) {
+      this.sizePag = cant;
+      this.pag = 0;
+      this.ejecutarConsultar();
+    }
+  }
+
+  // Método encargado de mostrar solo 5 paginaciones siguientes o anteriores en caso de existir muchas
+  mostrarPaginas(i: any): boolean {
+    const pagesToShow = 5;
+    const start = Math.max(0, this.pag - Math.floor(pagesToShow / 2));
+    const end = Math.min(this.totalPaginas.length, start + pagesToShow - 1);
+
+    if (i >= start && i <= end) {
+      return true;
+    }
+
+    return false;
   }
 
 }
